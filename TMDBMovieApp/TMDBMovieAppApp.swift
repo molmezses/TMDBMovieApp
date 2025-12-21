4bd7e874b95cdc83a11e5ffac2e26ac6fcdf319b
molmezses <mustafaolmezses@gmail.com>
2025-12-21 21:22:47 +0300

    Initial Commit

---
+++ TMDBMovieApp/TMDBMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBMovieAppApp.swift
+//  TMDBMovieApp
+//
+//  Created by mustafaolmezses on 21.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct TMDBMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
